5c9b0607d5cff9288a8f6a0a4b28577008730f26
limengcanyu <limengcanyu@163.com>
2023-09-23 14:12:41 +0800

    update

--- spring-boot-example/src/main/java/com/spring/boot/example/ExampleController.java
+++ spring-boot-example/src/main/java/com/spring/boot/example/ExampleController.java
@@ -1,9 +1,13 @@
 package com.spring.boot.example;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RequestMapping("/example")
@@ -11,14 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     /**
-     * http://localhost:8080/example/hello
+     * http://localhost:8091/example/hello
      *
      * @return
      */
     @GetMapping("/hello")
     public String hello() {
         log.info("call hello......");
-        return "example hello";
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
 }
